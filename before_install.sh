4ead29906a29409ab4934a786c0cfd36e7cb1488
aaidapa96 <96769025+aaidapa96@users.noreply.github.com>
2023-12-25 21:06:46 +0530

    Update before_install.sh

--- before_install.sh
+++ before_install.sh
@@ -4,7 +4,10 @@
 ECR_REPO_URI="373746118720.dkr.ecr.ap-south-1.amazonaws.com/owasp12"
 
 # AWS CLI login to ECR
-$(aws ecr get-login --no-include-email --region your-region)
+pip install --upgrade awscli
+aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION > docker_login.sh
+chmod +x docker_login.sh
+./docker_login.sh
 
 # Pull the Docker image
 docker pull 373746118720.dkr.ecr.ap-south-1.amazonaws.com/owasp12
