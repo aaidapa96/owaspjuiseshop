851e27c7b8c0deb7a5708d0f9ae3fefbd852b0ad
aaidapa96 <96769025+aaidapa96@users.noreply.github.com>
2023-12-26 18:33:27 +0530

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -4,3 +4,8 @@ docker rm mycont || true
 
 # Run the Docker container
 docker run -d -p 80:3000 --name mycont 373746118720.dkr.ecr.ap-south-1.amazonaws.com/owaspjuiceshoprepo:latest
+
+zap.sh -cmd -quickurl http://ec2-35-154-219-106.ap-south-1.compute.amazonaws.com -quickprogress -quickout ~/owasp_zap_report.html
+
+aws s3 cp out.xml s3://mubuck12
+
