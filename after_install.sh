9a5023ce0c4dd791da76896c4ec7d1af1b26b0a9
aaidapa96 <96769025+aaidapa96@users.noreply.github.com>
2023-12-26 14:14:46 +0530

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -1,11 +1,6 @@
-#!/bin/bash
-
-# Your ECR repository URI
-ECR_REPO_URI="373746118720.dkr.ecr.ap-south-1.amazonaws.com/owasp12"
-
 # Stop and remove the existing container
-docker stop your-container-name || true
-docker rm your-container-name || true
+docker stop mycont || true
+docker rm mycont || true
 
 # Run the Docker container
-docker run -d -p 80:80 --name your-container-name 373746118720.dkr.ecr.ap-south-1.amazonaws.com/owasp12
+docker run -d -p 80:3000 --name mycont 373746118720.dkr.ecr.ap-south-1.amazonaws.com/owasp12
