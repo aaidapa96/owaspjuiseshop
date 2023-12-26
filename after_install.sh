2e98e7afd8c0163abf576a34a928ac8aa604d834
aaidapa96 <96769025+aaidapa96@users.noreply.github.com>
2023-12-26 14:26:23 +0530

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -3,4 +3,4 @@ docker stop mycont || true
 docker rm mycont || true
 
 # Run the Docker container
-docker run -d -p 80:3000 --name mycont 373746118720.dkr.ecr.ap-south-1.amazonaws.com/owasp12
+docker run -d -p 80:3000 --name mycont 373746118720.dkr.ecr.ap-south-1.amazonaws.com/owaspjuiceshoprepo:latest
