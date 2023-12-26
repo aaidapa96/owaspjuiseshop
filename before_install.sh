8ab51d1c2d977225fed293dd574e9db71e39706b
aaidapa96 <96769025+aaidapa96@users.noreply.github.com>
2023-12-26 14:27:20 +0530

    Update before_install.sh

--- before_install.sh
+++ before_install.sh
@@ -2,7 +2,7 @@ sudo service codedeploy-agent start
 sudo service docker start
 
 sudo usermod -aG docker $USER
-sudo aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 373746118720.dkr.ecr.ap-south-1.amazonaws.com/owasp12:latest > docker_login.sh
+sudo aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 373746118720.dkr.ecr.ap-south-1.amazonaws.com/owaspjuiceshoprepo:latest > docker_login.sh
 chmod +x docker_login.sh
 
-docker pull 373746118720.dkr.ecr.ap-south-1.amazonaws.com/owasp12
+docker pull 373746118720.dkr.ecr.ap-south-1.amazonaws.com/owaspjuiceshoprepo:latest
